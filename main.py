d72c59396f96301b97b8426771d795b5d2e6e897
INFJakZda <zdanowski78@gmail.com>
2019-03-27 22:48:57 +0100

    Add read from args

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-
+import sys
+import os
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) != 3:
+        print(os.path.basename(sys.argv[0]) + " <path> <N>")
+        exit(1)
+
+    data_path = os.path.abspath(sys.argv[1])
+    N = (int(sys.argv[2]))
+    
+    print(data_path)
+    print(N)
